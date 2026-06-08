9838168936910ae262cf1024d175777e86ced3e2
yzf12346 <2998127301@qq.com>
2026-06-08 23:47:15 +0800

    更新

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import {defineConfig} from 'vite'
+import {svelte} from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte(),tailwindcss()],
+  plugins: [svelte(), tailwindcss(),
+  ],
+  base: "/QingJiao",
+  build: {outDir: "docs"},
 })
 
 
