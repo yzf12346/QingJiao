2a1f68e9c0bb591d134ed538d0e2b9b4a36c86cb
yzf12346 <2998127301@qq.com>
2026-06-09 15:33:38 +0800

    移除router

--- src/App.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  export const prerender = true;
-  import { Router } from "@mateothegreat/svelte5-router";
-  import Home from "./routes/Home.svelte";
-
-  const routes = [{ component: Home }];
-</script>
-
-<Router {routes} basePath="/QingJiao" />
